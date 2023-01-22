bee24ef7506a05127044c35f457c5d8163198e6b
José Domingos <domliraa@gmail.com>
2023-01-22 13:06:59 -0300

    add habits infos descrition popover and add css transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './libs/dayjs';
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { Api } from "../libs/axios";
 import { generateDatesFromYearBeginning } from "../utils/generete-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
@@ -16,7 +19,22 @@ const summaryDays = generateDatesFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 7
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDays.length
 
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([])
+
+    useEffect(() => {
+        Api.get('summary').then(response => {
+            setSummary(response.data)
+        })
+    }, [])
+
     return (
         <div className='w-full flex'>
             <div className='grid grid-rows-7 grid-flow-row gap-3'>
@@ -28,8 +46,16 @@ export function SummaryTable() {
             </div>
 
             <div className='grid grid-rows-7 grid-flow-col gap-3'>
-                {summaryDays.map(date => {
-                    return <HabitDay key={date.toString()} />
+                {summary.length > 0 && summaryDays.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
+                    return <HabitDay
+                        key={date.toString()}
+                        date={date}
+                        amount={dayInSummary?.amount}
+                        defaultCompleted={dayInSummary?.completed}
+                    />
                 })}
 
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
